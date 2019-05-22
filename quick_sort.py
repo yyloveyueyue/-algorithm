bd8c5186c58727dbc6ed47fc76c4fb9cf2500f96
yuan ye <974972165@qq.com>
2019-05-22 21:07:49 +0800

    mergeSort_2

--- quick_sort.py
+++ quick_sort.py
@@ -1,6 +1,6 @@
 # -*- coding:utf-8 -*-
 
-
+# 方法1
 def quick_sort(arr, left=None, right=None):
     left = 0 if not isinstance(left,(int, float)) else left
     right = len(arr)-1 if not isinstance(right,(int, float)) else right
@@ -19,17 +19,52 @@ def partition(arr, left, right):
         if arr[i] < arr[pivot]:
             swap(arr, i, index)
             index+=1
-        i+=1
+        i += 1
     swap(arr,pivot,index-1)
     return index-1
 
 def swap(arr, i, j):
     arr[i], arr[j] = arr[j], arr[i]
 
+# 方法2
+def quick_sort_v2(alist, fst, lst):
+    """
+    快速排序
+
+    :param alist: 待排序列表
+    :param fst: 起始idx
+    :param lst: 终止idx
+    :return: None
+    """
+    if fst >= lst:
+        return
+    pivot = alist[fst]
+    index = fst + 1
+    i = index
+    while i <= lst:
+        if alist[i] < pivot:
+            alist[i], alist[index] = alist[index], alist[i]
+            index += 1
+            print(alist)
+        i += 1
+    # p_idx = min(i, j)  # 枢轴索引
+    alist[fst], alist[index-1] = alist[index-1], alist[fst]
+    print(alist)
+    quick_sort_v2(alist, fst, index - 2)
+    quick_sort_v2(alist, index, lst)
+
+
+
+
+
 
 def test_of_quicj_sort():
-    alist = [54, 26, 93, 17, 77, 31, 44, 55, 20]
-    quick_sort(alist)
+    alist = [54, 26, 93, 17, 77, 56, 26, 55, 20]
+    # quick_sort(alist)
+    print(alist)
+    fst1 = 0
+    lst1 = len(alist) - 1
+    quick_sort_v2(alist,fst1,lst1)
     print(alist)
 
 
