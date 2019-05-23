31ef4de16ac74051715b9f1eb0868600961a6dd4
yuan ye <974972165@qq.com>
2019-05-23 14:12:14 +0800

    quick sort 修改

--- quick_sort.py
+++ quick_sort.py
@@ -54,6 +54,36 @@ def quick_sort_v2(alist, fst, lst):
     quick_sort_v2(alist, index, lst)
 
 
+# 方法3
+def QuickSort(myList,start,end):
+    #判断low是否小于high,如果为false,直接返回
+    if start < end:
+        i,j = start,end
+        #设置基准数
+        base = myList[i]
+
+        while i < j:
+            #如果列表后边的数,比基准数大或相等,则前移一位直到有比基准数小的数出现
+            while (i < j) and (myList[j] >= base):
+                j = j - 1
+
+            #如找到,则把第j个元素赋值给第个元素i,此时表中i,j个元素相等
+            myList[i] = myList[j]
+            print(myList)
+
+            #同样的方式比较前半区
+            while (i < j) and (myList[i] <= base):
+                i = i + 1
+            myList[j] = myList[i]
+            print(myList)
+        #做完第一轮比较之后,列表被分成了两个半区,并且i=j,需要将这个数设置回base
+        myList[i] = base
+        print(myList)
+
+        #递归前后半区
+        QuickSort(myList, start, i - 1)
+        QuickSort(myList, j + 1, end)
+    return myList
 
 
 
@@ -64,7 +94,7 @@ def test_of_quicj_sort():
     print(alist)
     fst1 = 0
     lst1 = len(alist) - 1
-    quick_sort_v2(alist,fst1,lst1)
+    QuickSort(alist,fst1,lst1)
     print(alist)
 
 
